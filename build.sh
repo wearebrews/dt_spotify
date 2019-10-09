3091329e836790e06ff7b7d4d670eb2021b848f7
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-10-09 21:15:48 +0200

    Update build

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+VERSION = $(git rev-parse HEAD)
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 make docker-push
+
+kubectl set image -n dtspotify deployment/dt-spotify dtspotify=wearebrews/dtspotify:$VERSION 
+
