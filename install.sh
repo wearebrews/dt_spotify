8bd50690d129af077f5ad08a276f08dcf6a5476c
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-10-09 20:41:10 +0200

    Add travis support

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+
+#Install doctl
+DOCTL_VERSION=1.17.0
+curl -sL https://github.com/digitalocean/doctl/releases/download/v$DOCTL_VERSION/doctl-$DOCTL_VERSION-linux-amd64.tar.gz | tar -xzv
+sudo mv doctl /usr/local/bin
+
+#Requires a valid access token in env
+doctl kubernetes cluster kubeconfig save brews-k8s
+
+
+
+
+
