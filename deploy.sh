be839e6850ba4cd341d0661dfc36f19c8af6d758
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-10-15 20:14:15 +0200

    Fix build

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 VERSION=$(git rev-parse HEAD)
 NAMESPACE=dtspotify
 make docker-push
-kubectl apply -n $NAMESPACE -f kubernetes/deployment.yaml
+kubectl apply -n $NAMESPACE -f kubernetes/
 kubectl set image -n $NAMESPACE deployment/dt-spotify dtspotify=wearebrews/dtspotify:$VERSION 
 
