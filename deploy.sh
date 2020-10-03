6157ac75aab0611e67d0b220a71cd62457bdfe8a
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2020-10-03 18:35:23 +0200

    Use google cloud

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,14 @@ VERSION=$(git rev-parse HEAD)
 NAMESPACE=dtspotify
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 make docker-push
+
+
+GCLOUD_CREDENTIALS="$PWD/client-secret.json"
+echo "$GCLOUD_KEY" > "${GCLOUD_CREDENTIALS}"
+
+gcloud auth activate-service-account --key-file "${GCLOUD_CREDENTIALS}"
+gcloud container clusters get-credentials brews-k8s --zone europe-north1-a --project wearebrews
+
 kubectl apply -n $NAMESPACE -f kubernetes/
 kubectl set image -n $NAMESPACE deployment/dt-spotify dtspotify=wearebrews/dtspotify:$VERSION 
 
--- install.sh
+++ install.sh
@@ -7,14 +7,12 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 
 mkdir ${HOME}/.kube
 
-#Install doctl
-DOCTL_VERSION=1.17.0
-curl -sL https://github.com/digitalocean/doctl/releases/download/v$DOCTL_VERSION/doctl-$DOCTL_VERSION-linux-amd64.tar.gz | tar -xzv
-sudo mv doctl /usr/local/bin
-
-#Requires a valid access token in env
-doctl kubernetes cluster kubeconfig show brews-k8s > ${HOME}/.kube/config
-
+if [ ! -f "$GCLOUD_PATH_APPLY" ]; then
+  echo Installing gcloud SDK
+  rm -rf "$GCLOUD_HOME"
+  export CLOUDSDK_CORE_DISABLE_PROMPTS=1 # SDK installation is interactive, thus prompts must be disabled
+  curl "https://sdk.cloud.google.com" | bash > /dev/null
+fi
 
 
 
